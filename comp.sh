f8a899e4d9c9f32a32ab7e4d1e59520b195959f1
pokengineer <vfacuu@gmail.com>
2018-06-08 17:31:11 -0300

    compiler script para agilizar las cosas

---
+++ comp.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# COMPILADOR DEL SERVIDOR y CLIENTE, DEBE ESTAR EN LA MISMA CARPETA DE LOS ARCHIVOS .C
+gcc preguntanosPrueba.c -lpthread -o preg
+#Ejemplo de ejecuccion ./scriptCompilador
